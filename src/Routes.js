2a2421c5934e3d62d0e00ab107fe3b491e96fd56
Miguel Soares <miguelanjohps@gmail.com>
2022-11-09 18:04:44 -0300

    feat: Cria página de Login e validação de inputs

--- src/Routes.js
+++ src/Routes.js
@@ -1,9 +1,13 @@
 import React, { Component } from 'react';
+import { Route, Switch } from 'react-router-dom';
+import Login from './pages/Login';
 
 export default class Routes extends Component {
   render() {
     return (
-      <div>Routes</div>
+      <Switch>
+        <Route exact path="/" component={ Login } />
+      </Switch>
     );
   }
 }
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,9 +1,55 @@
 import React, { Component } from 'react';
 
 class Login extends Component {
+  state = {
+    name: '',
+    email: '',
+  };
+
+  handleInput = ({ target: { id, value } }) => this.setState({ [id]: value });
+
+  validateInputs = (name, email) => {
+    const emailRegex = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i;
+    return emailRegex.test(email) && name.trim().length >= 2;
+  };
+
   render() {
+    const { handleInput, validateInputs, state: { name, email } } = this;
+    const isDisabled = !validateInputs(name, email);
     return (
-      <div />
+      <div>
+        <form>
+          <label htmlFor="name">
+            <input
+              type="text"
+              data-testid="input-player-name"
+              id="name"
+              value={ name }
+              onChange={ handleInput }
+              placeholder="Nome"
+            />
+          </label>
+          <p />
+          <label htmlFor="email">
+            <input
+              type="email"
+              data-testid="input-gravatar-email"
+              id="email"
+              value={ email }
+              onChange={ handleInput }
+              placeholder="E-mail"
+            />
+          </label>
+          <p />
+          <button
+            type="button"
+            data-testid="btn-play"
+            disabled={ isDisabled }
+          >
+            Play
+          </button>
+        </form>
+      </div>
     );
   }
 }
