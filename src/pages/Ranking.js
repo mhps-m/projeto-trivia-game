caa311a60bc9e8c773bf790860e1eeaf4b36e9cd
Miguel Soares <miguelanjohps@gmail.com>
2022-11-12 18:48:33 -0300

    feat: Cria botão Home na página de ranking para redirecionar à tela inicial

--- src/pages/Ranking.js
+++ src/pages/Ranking.js
@@ -2,12 +2,24 @@ import React, { Component } from 'react';
 
 class Ranking extends Component {
   render() {
+    const { history } = this.props;
     return (
-      <h1 data-testid="ranking-title">
-        Título
-      </h1>
+      <div>
+        <h1 data-testid="ranking-title">
+          Título
+        </h1>
+        <button
+          type="button"
+          data-testid="btn-go-home"
+          onClick={ () => history.push('/') }
+        >
+          Home
+        </button>
+      </div>
     );
   }
 }
+
+Ranking.propTypes = {}.isRequired;
 
 export default Ranking;
